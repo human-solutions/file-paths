853b50f3ab642906789526073c8ffe22c35f21a0
hakesson <henrik@akesson.mobi>
2023-03-10 06:55:38 +0100

    More spec

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,15 @@
 //!     - `~/folder1` becomes `/<user-home-dir>/folder1`.
 //!     - `./folder1/$MY_DIR` becomes `./folder1/dir1` when `$MY_DIR` is `dir1`
 //!     - `folder1` becomes `./folder1`
-//!     - `.literal(..)` if you really need to skip resolution.
+//!     - `.*_lit(..)` functions if you really need to skip resolution and create a path like `./folder1/"one/name"/"$HOME"`
+//!     - `c:\\folder1` becomes `c:/folder1`
 //! - Path resolution without file-system access so that paths don't need to exist.
 //! - Comparable paths (because they are resolved, see above).
-//! - Error handling with [anyhow](https://crates.io/crates/anyhow) aims to produce comprehensive human-readable messages instead of machine-parsable ones.
+//! - Displays resolved paths or use `.native_string()` or `format("{path:#}")` for outputting OS native string.
+//! - Error:
+//!     - handling with [anyhow](https://crates.io/crates/anyhow) aims to produce comprehensive human-readable messages instead of machine-parsable ones.
+//!     - the message always includes the path in question.
+//!     - the message includes the current working directory for relative paths.
 //! - Support for the major operating systems: Linux, Unix, macOS and Windows.
 //!
 //! Non-goals:
